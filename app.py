f88f1f4c984f398ad5e19a4919d3885b01905d4e
!(geek) <93247093+Sadhana1842@users.noreply.github.com>
2025-01-24 10:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 # Load pre-trained fine-tuned model and tokenizer from Hugging Face's public model hub
-model_name = "Respair / deberta-v3-large-finetuned-style"  # DeBERTa model fine-tuned on IMDB sentiment analysis
+model_name = "Respair/deberta-v3-large-finetuned-style"  # Using Respair's fine-tuned DeBERTa model
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
